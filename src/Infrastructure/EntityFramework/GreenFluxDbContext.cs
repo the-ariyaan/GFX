1f639c3f4d1ac1c692802682bae07cdb17128ce4
MohammadReza Ariyan <the.ariyaan@gmail.com>
2022-05-19 20:02:16 +0430

    dbcontext and models has been added

---
+++ src/Infrastructure/EntityFramework/GreenFluxDbContext.cs
@@ -0,0 +1,32 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.EntityFramework;
+
+public class GreenFluxDbContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public GreenFluxDbContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public virtual ICollection<ChargeStation> ChargeStations { get; set; }
+    public virtual ICollection<Connector> Connectors { get; set; }
+    public virtual ICollection<Group> Groups { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //The schema for the database
+        const string schema = "gfx";
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to sql server with connection string from app settings
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+    }
+}
